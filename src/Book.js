2478216f45d6865eed3776c410ad6c73ddeca6b9
Chilja Speransky <magiclanzelot@gmx.net>
2018-07-20 11:35:08 +0200

    Seperation of the content into components

--- src/Book.js
+++ src/Book.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import ChangeBookshelf from './ChangeBookshelf';
 
 class Book extends Component {
 
   render() {
     return(
-      <div></div>
+      <div className="book">
+        <div className="book-top" style={{ backgroundImage: '' }}>
+          <div className="book-cover"/>
+          <ChangeBookshelf/>
+        </div>
+        <div className="book-title">Book Title</div>
+        <div className="book-authors">Author</div>
+      </div>
     )
   }
 }
--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import Book from './Book';
 
 class Bookshelf extends Component {
 
   render() {
     return(
-      <div></div>
+      <div>
+        <div className="bookshelf">
+          <h2 className="bookshelf-title">Title</h2>
+          <div className="bookshelf-books">
+            <ol className="books-grid">
+              <Book/>
+            </ol>
+          </div>
+        </div>
+      </div>
     )
   }
 }
